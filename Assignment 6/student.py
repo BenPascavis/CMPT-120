1899a391f0a3acbe925aac67ba4a8d17a373850d
BenPascavis <143512417+BenPascavis@users.noreply.github.com>
2023-11-12 15:44:39 -0500

    student class update

--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -16,23 +16,49 @@ create a function to see if the student is eligible for the honors program
 to be eligible they need to have a gpa above 3.5
 return true if they can and false if they cant, and print it out
 
-create a function because this college is a wacky one- every day they generate a student id and if the student id matches a student, that student gets free food that day. 
+create a function because this college is a wacky one- every day they generate a student id and if the student id matches a student,
+that student gets free food that day.
 1. generate a random number the length of however long you choose to make the id number
 2. compare if the random number matches your student's id
 3. if it matches print out "winner! student (name) gets free lunch!"
 4. if not, print "Loser!"
-(disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
+(disclosure: obviously there's a very small chance of your generated number matching the student id number.
+I just want to see that you're generating and comparing properly)
 '''
 
-class student:
+import random
+class Student:
+    def __init__ (self, name, studentID, year, major, gpa):
+        self.name = name
+        self.studentID = studentID
+        self.year = year
+        self.major = major
+        self.gpa = gpa
+
+
     
     
-    
-    
-    
-    
+def freeLunch(self):
+    num = random.randint(1000,9999)
+    if (self.studentID == num):
+        return print("Winner!",self.name, "gets free lunch!")
+    else:
+        return print("Loser!")
+def honors(self):
+    if (self.gpa>3.5):
+        return True
+    else:
+        return False
 def main():
     #create three students and check if they get free lunch and if they qualify for honors
-    
+    student1 = Student(name="Bob", studentID=9999, year="s", major="compsci", gpa=3.7)
+    student2 = Student(name="Elena", studentID=6669, year="f", major="Econ", gpa=3.8)
+    student3 = Student(name="Sam", studentID=1001, year="f", major="communications", gpa = 2.3)
+    print(honors(student1))
+    print(honors(student2))
+    print(honors(student3))
+    freeLunch(student1)
+    freeLunch(student2)
+    freeLunch(student3)
     
 main()
